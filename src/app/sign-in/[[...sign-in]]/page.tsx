17bfabd88cacca50b05293016399544bbbd20a2c
Atul Bora <atulbora250@gmail.com>
2026-03-05 23:52:35 +0530

    changes

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,18 @@
-import { SignIn } from '@clerk/nextjs'
+'use client'
+import { SignIn, useUser } from '@clerk/nextjs'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function Page() {
+  const { isSignedIn } = useUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.replace("/dashboard")
+    }
+  }, [isSignedIn, router])
+  
   return (
     <div className="flex min-h-screen items-center justify-center">
       <SignIn routing="path" path="/sign-in" />
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,17 @@
-import { SignUp } from '@clerk/nextjs'
+'use client'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { SignUp, useUser } from '@clerk/nextjs'
 
 export default function Page() {
+  const { isSignedIn } = useUser()
+    const router = useRouter()
+  
+    useEffect(() => {
+      if (isSignedIn) {
+        router.replace("/dashboard")
+      }
+    }, [isSignedIn, router])
   return (
     <div className="flex min-h-screen items-center justify-center">
       <SignUp routing="path" path="/sign-up"  />
